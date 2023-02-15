8bb3fd6f7cdb2a21b1177d9c51b28047ba4bc452
tayn0 <tristan.young94@gmail.com>
2023-02-14 21:03:00 -0600

    added lua template

---
+++ isdf01/isdf01.lua
@@ -0,0 +1,65 @@
+--[[
+
+THIS IS A TEST
+Event Scripting: 
+Ported to LUA: 
+Date: 7-11-2021
+Description: Campagin mission  ported to LUA
+References:
+1. https:--steamcommunity.com-sharedfiles-filedetails-?id=1488402495
+2. https:www.lua.org-docs.html
+]] --
+local M = {
+
+   
+} --End Mission
+
+function Save()
+   return Mission
+end
+
+function Load(...)
+   if select("#", ...) > 0 then
+      Mission = ...
+   end
+end
+
+function Start() --This function is called upon the first frame
+
+	M.player = GetPlayerHandle()
+	
+
+end
+
+function AddObject(h) --This function is called when an object appears in the game. --
+
+
+	
+end
+
+
+function DeleteObject(h) --This function is called when an object is deleted in the game.
+end
+
+function InitialSetup()
+Mission.TPS = EnableHighTPS()
+AllowRandomTracks(true)
+end
+
+
+
+function Update() --This function runs on every frame.
+Mission.TurnCounter = Mission.TurnCounter + 1
+
+missionCode() --Calling our missionCode function in Update.
+end
+
+function missionCode() --
+
+	Mission.player = GetPlayerHandle()
+	
+
+
+
+ 	
+end
